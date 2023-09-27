361ec8da7f5bffdfd29b46b81bfbca652c70d543
mateuvrs <mateusvrs.dev@gmail.com>
2023-09-27 01:28:44 -0300

    api(core): move arquivos e pastas para o core
    - O nome da pasta api foi alterado para core
    - Foi iniciado um app chamado api

--- api/manage.py
+++ api/manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from decouple import config
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('SETTINGS_FILE_PATH'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
