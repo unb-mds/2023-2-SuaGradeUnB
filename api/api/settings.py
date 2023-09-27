d1e96b3350edc11fd1806a72c2a12fff15ed34eb
Caio <caiofeliperro@gmail.com>
2023-09-26 21:51:13 -0300

    django(api): autoriza acesso de qualquer origem
    Co-authored-by: mateusvrs <mateusvrs.dev@gmail.com>

--- api/api/settings.py
+++ api/api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
