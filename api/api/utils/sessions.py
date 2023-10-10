f77cf27c983835f98191fe39482c8c51fa6d3341
GabrielCastelo-31 <gabflubr@outlook.com>
2023-10-09 23:07:51 -0300

    Comments have been added to the code.

--- api/api/utils/sessions.py
+++ api/api/utils/sessions.py
@@ -6,8 +6,9 @@ import requests.cookies
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
-# 
+# The url of the web page
 URL = "https://sigaa.unb.br/sigaa/public/turmas/listar.jsf"
+# The headers of the request
 HEADERS = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET',
@@ -16,7 +17,7 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
 }
 
-
+'''Create a request session with retry and backoff_factor'''
 def create_request_session() -> requests.Session:
     session = requests.Session()
     retry = Retry(connect=3, backoff_factor=0.5)
@@ -24,14 +25,14 @@ def create_request_session() -> requests.Session:
     session.mount('https://', adapter)
     return session
 
-
+'''Get the response from the request session'''
 def get_response(session: requests.Session) -> requests.Response:
     response = session.get(url=URL, headers=HEADERS)
     return response
 
 
 def get_session_cookie(session: requests.Session) -> requests.cookies.RequestsCookieJar:
-
+    '''Get the cookie from the request session'''
     response = get_response(session)
     cookie = response.cookies.get_dict()
     cookie_jar = requests.cookies.RequestsCookieJar()
@@ -40,6 +41,7 @@ def get_session_cookie(session: requests.Session) -> requests.cookies.RequestsCo
 
 
 def get_current_year_and_period():
+    '''Get the current year and period'''
     today_date = datetime.now()
     current_year = today_date.year
     if datetime(current_year, 5, 1) <= today_date <= datetime(current_year, 10, 30):
--- api/api/utils/web_scraping.py
+++ api/api/utils/web_scraping.py
@@ -8,6 +8,7 @@ import requests.utils
 
 
 def get_list_of_departments():
+    '''Get the list of departments'''
     response = sessions.get_response(create_request_session())
     soup = BeautifulSoup(response.content, "html.parser")
     departments = soup.find("select", attrs={"id": "formTurma:inputDepto"})
@@ -21,12 +22,16 @@ def get_list_of_departments():
 
 
 class DisciplineWebScraper:
+    '''Class to make the web scraping of disciplines'''
+
     def __init__(self, department: str, year: str, period: str, session=None, cookie=None):
-        self.url = URL
-        self.department = department
+        '''Constructor of the class DisciplineWebScraper'''
+
+        self.url = URL  # The url of the web page
+        self.department = department  # The department code
         self.year = year
-        self.period = period
-        self.data = {
+        self.period = period  # 1 for first semester and 2 for second semester
+        self.data = {  # This data is necessary to make the post request
             "formTurma":	"formTurma",
             "formTurma:inputNivel":	"",
             "formTurma:inputDepto":	self.department,
@@ -36,21 +41,25 @@ class DisciplineWebScraper:
             "javax.faces.ViewState":	"j_id1"}
 
         if session is None:
-            self.session = sessions.create_request_session()
+            self.session = sessions.create_request_session()  # Create a request session
         else:
             self.session = session
         if cookie is None:
+            # Get the cookie from the request session
             self.cookie = sessions.get_session_cookie(self.session)
         else:
             self.cookie = cookie
 
-    def get_response_from_disciplines_post_request(self):
+    def get_response_from_disciplines_post_request(self) -> requests.Response:
+        '''Make a post request to get the response of the disciplines's classes available'''
+
         response = self.session.post(
             url=self.url, headers=HEADERS, cookies=self.cookie, data=self.data)
         return response
 
     def make_web_scraping_of_disciplines(self, response):
         '''Still necessary to implement this method'''
+
 
 """
 This is a test of the DisciplineWebScraper class
