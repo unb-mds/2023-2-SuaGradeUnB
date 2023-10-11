767574ae89e1f10950882a51277984b3a426cfd1
GabrielCastelo-31 <gabflubr@outlook.com>
2023-10-10 23:17:46 -0300

    Web Scraping prototype added. Data collected

--- api/api/utils/web_scraping.py
+++ api/api/utils/web_scraping.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+from collections import defaultdict
 import sessions
 from sessions import create_request_session
 from sessions import URL
@@ -25,7 +27,7 @@ class DisciplineWebScraper:
 
     def __init__(self, department: str, year: str, period: str, session=None, cookie=None):
         '''Constructor of the class DisciplineWebScraper'''
-
+        self.disciplines = defaultdict(list)  # A dictionary with the disciplines
         self.url = URL  # The url of the web page
         self.department = department  # The department code
         self.year = year
@@ -49,6 +51,10 @@ class DisciplineWebScraper:
         else:
             self.cookie = cookie
 
+        """ response = self.get_response_from_disciplines_post_request()
+        discipline_table = self.make_web_scraping_of_disciplines(response) """
+
+
     def get_response_from_disciplines_post_request(self) -> requests.Response:
         '''Make a post request to get the response of the disciplines's classes available'''
 
@@ -57,14 +63,28 @@ class DisciplineWebScraper:
         return response
 
     def make_web_scraping_of_disciplines(self, response):
-        '''Still necessary to implement this method'''
+        '''Make the web scraping of the disciplines'''
+        soup = BeautifulSoup(response.content, "html.parser")
+        table = soup.find("table", attrs={"class": "listagem"}) # Find the <table> tag with class "listagem"
+        tr = table.find_all("tr") # Find all <tr> tags
+        """ print(tr[0])
+        print(tr[1])
+        print(tr[2]) """
+        aux_title_and_code = ""
+        if tr is not None:
+            for discipline in tr:
+                if discipline.find("span", attrs={"class": "tituloDisciplina"}) is not None: #Verify if the <tr> tag has a <span> tag with class "tituloDisciplina"
+                    title = discipline.find("span", attrs={"class": "tituloDisciplina"})# Find the <span> tag with class "tituloDisciplina"
+                    aux_title_and_code = title.get_text().strip('-')
+                elif "linhaPar" in discipline.get("class", []) or "linhaImpar" in discipline.get("class", []):
+                    discipline_treated = aux_title_and_code.split(' - ') # Split the title and code of the discipline to get the code and name
 
-
-"""
-This is a test of the DisciplineWebScraper class
-
-department = get_list_of_departments()
-webscrap = DisciplineWebScraper(department[2], "2023", "2")
-discipline = webscrap.get_response_from_disciplines_post_request()
-webscrap.make_web_scraping_of_disciplines(discipline)
- """
+                    discipline_code = discipline_treated[0]
+                    discipline_name = discipline_treated[1]
+                    table_data = discipline.find_all("td") # Find all <td> tags
+                    class_code = int(table_data[0].get_text()) # Find the <td> tag with class "turma"
+                    teacher_name_with_hours = discipline.find("td",attrs={"class":"nome"}).get_text().strip().strip('\n').split(' ') # Find the <td> tag with class "professor"
+                    teacher_name = ' '.join(teacher_name_with_hours[:-1])
+                    class_workload = teacher_name_with_hours[-1].replace(('('), '').replace((')'), '')
+                    classroom = table_data[7].get_text() # Find the <td> tag with class "sala"
+                    schedule = table_data[3].get_text().strip().strip('\n').strip('\t').strip('\r') # Find the <td> tag with class "horario"
