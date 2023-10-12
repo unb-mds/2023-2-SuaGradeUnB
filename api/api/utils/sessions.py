c3b630cf1f8a7ada19ea043602513dfd2e3d5cd4
Caio <caiofeliperro@gmail.com>
2023-10-12 20:16:08 -0300

    api(sessions): melhora legibilidade do código

--- api/api/utils/sessions.py
+++ api/api/utils/sessions.py
@@ -1,13 +1,10 @@
-from datetime import datetime
-import requests
-import requests.utils
-import requests.cookies
+from requests import Session, Response, cookies
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+from datetime import datetime
+from typing import List
 
-# The url of the web page
 URL = "https://sigaa.unb.br/sigaa/public/turmas/listar.jsf"
-# The headers of the request
 HEADERS = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET',
@@ -17,35 +14,37 @@ HEADERS = {
 }
 
 '''Create a request session with retry and backoff_factor'''
-def create_request_session() -> requests.Session:
-    session = requests.Session() # Create a persistent request session
+def create_request_session() -> Session:
+    session = Session() # Create a persistent request session
     retry = Retry(connect=3, backoff_factor=0.5) # Create a retry object
     adapter = HTTPAdapter(max_retries=retry) # Try to make the request 3 times
     session.mount('https://', adapter) # Mount the adapter to the session
+
     return session
 
 '''Get the response from the request session'''
-def get_response(session: requests.Session) -> requests.Response:
+def get_response(session: Session) -> Response:
     response = session.get(url=URL, headers=HEADERS) # Make a get request to the url
+
     return response
 
-
-def get_session_cookie(session: requests.Session) -> requests.cookies.RequestsCookieJar:
+def get_session_cookie(session: Session) -> cookies.RequestsCookieJar:
     '''Get the cookie from the request session'''
     response = get_response(session) # Get the response from the request session
     cookie = response.cookies.get_dict() # Get the cookie from the response
-    cookie_jar = requests.cookies.RequestsCookieJar() # Create a cookie jar
+    cookie_jar = cookies.RequestsCookieJar() # Create a cookie jar
     cookie_jar.update(cookie) # Update the cookie jar with the cookie
+
     return cookie_jar
 
+def get_current_year_and_period() -> List[int, str]:
+    # Pega o ano e o período atual
+    current_date = datetime.now() 
+    current_year = current_date.year
+    period = "1"
 
-def get_current_year_and_period() -> list:
-    '''Get the current year and period'''
-    today_date = datetime.now() # Get the current date
-    current_year = today_date.year # Get the current year
-    if datetime(current_year, 5, 1) <= today_date <= datetime(current_year, 10, 30):
-        '''If the current date is between May 1 and October 30, the period is 2, else the period is 1'''
+    # Se a data atual estiver entre 1 de maio e 30 de outubro, o período é 2.
+    if datetime(current_year, 5, 1) <= current_date <= datetime(current_year, 10, 30):
         period = "2"
-    else:
-        period = "1"
+
     return [current_year, period]
