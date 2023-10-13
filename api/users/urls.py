98b533b0f0ac5fee6ab8f70629a503f5341bee54
mateuvrs <mateusvrs.dev@gmail.com>
2023-10-13 18:47:18 -0300

    api(urls): adição do app users

---
+++ api/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import re_path, path
+from users import views
+
+app_name = 'users'
+
+urlpatterns = [
+    re_path('register/' + r'(?P<oauth2>[^/]+)/$', views.Register.as_view(), name='register'),
+    path('login/', views.RefreshJWTView.as_view(), name='login'),
+]
