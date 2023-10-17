84364fdd414e940a36dea69961dccfb46b0ddc50
MateusVrs <mateusvrs.dev@gmail.com>
2023-10-17 14:22:21 -0300

    scripts(env): script para gerar chave django

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,4 +2,4 @@
 
 cp ./api/.env.example ./api/.env
 
-sed -i "s/your_secret_key/$(python3 -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')/g" ./api/.env
+sed -i "s/your_secret_key/$(python3 -c 'import string; import random; print("".join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(64)))')/g" ./api/.env
