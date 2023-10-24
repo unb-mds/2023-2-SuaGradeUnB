05da0099ad25147196b5c6db55768c158f53e9b1
Henrique Quenino <hcquenino@gmail.com>
2023-10-24 16:37:02 -0300

    Testando o Login do Usuario, e token de acesso

--- api/users/tests.py
+++ api/users/tests.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class UserSessionLoginTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.token = "test_token"
+        
+    def verify_token(self, token):
+        self.assertEqual(token, self.token)
+        
+    def test_login(self):
+        self.client.force_authenticate(user=None, token=None)
+        response = self.client.post(reverse('users:login'), {'access_token': self.token})
+        self.assertEqual(response.status_code, 200)
+        self.verify_token(response.data['access'])
+    
+    
+    
