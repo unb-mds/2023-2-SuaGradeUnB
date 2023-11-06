66dd5bdb97748bb1356aa843c2db2b99050d76a6
GabrielCastelo-31 <gabflubr@outlook.com>
2023-11-05 22:07:06 -0300

    Models updated. Class class added.
    Co-authored-by: Caio Felipe <caiofeliperro@gmail.com>

--- api/api/models.py
+++ api/api/models.py
@@ -3,23 +3,30 @@ from django.contrib.postgres.fields import ArrayField
 
 class Department(models.Model):
     # Classe que representa um departamento
-    name = models.CharField(max_length=50, unique=True)
     code = models.CharField(max_length=10, unique=True)
+    year = models.CharField(max_length=4, default='0000')
+    period = models.CharField(max_length=1, default='1')
 
     def __str__(self):
-        return self.name
+        return self.code
 
 class Discipline(models.Model):
     # Classe que representa uma disciplina
     name = models.CharField(max_length=50)
     code = models.CharField(max_length=10, unique=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='disciplines')
+
+    def __str__(self):
+        return self.name
+
+class Class(models.Model):
     workload = models.IntegerField()
     teachers = ArrayField(models.CharField(max_length=50))
     classroom = models.CharField(max_length=50)
     schedule = models.CharField(max_length=50)
     days = ArrayField(models.CharField(max_length=50))
     _class = models.IntegerField()
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='disciplines')
+    discipline = models.ForeignKey(Discipline, on_delete=models.CASCADE, related_name='classes')
 
     def __str__(self):
-        return self.name
+        return self._class
