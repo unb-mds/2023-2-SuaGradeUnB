6fc718b0de36a3b00bb7fea11fd1c11134b581e7
GabrielCastelo-31 <gabflubr@outlook.com>
2023-11-06 11:43:29 -0300

    Finished updatedb command line function.
    Co-authored-by: Caio Felipe <caiofeliperro@gmail.com>

--- api/api/management/commands/updatedb.py
+++ api/api/management/commands/updatedb.py
@@ -1,26 +1,75 @@
 from typing import Any
 from django.core.management.base import BaseCommand
-from api.utils import sessions, web_scraping
+from api.utils import sessions, web_scraping, db_handler
 from api.models import Discipline, Department
 from decouple import config
+from time import time
 
-"""Comando para atualizar o banco de dados."""
+
 class Command(BaseCommand):
+    """Comando para atualizar o banco de dados."""
+
     help = "Atualiza o banco de dados com as disciplinas do SIGAA e suas respectivas turmas."
 
     def handle(self, *args: Any, **options: Any):
-        departments_ids = web_scraping.get_list_of_departments() #Obtem os códigos dos departamentos
+        departments_ids = web_scraping.get_list_of_departments()
 
         if departments_ids is None:
-            self.display_error_message("department_ids") #Exibe uma mensagem de erro caso não seja possível obter os códigos dos departamentos
+            self.display_error_message("department_ids")
             return
 
         # Obtem o ano e o período atual e o ano e o período seguinte
+        previous_period_year, previous_period = sessions.get_previous_period()
         current_year, current_period = sessions.get_current_year_and_period()
         next_period_year, next_period = sessions.get_next_period()
 
+        # Apaga as disciplinas do período interior
+        db_handler.delete_all_departments_using_year_and_period(previous_period_year, previous_period)
+
+        # Pega a lista de departamentos do web scraping
+        departments_ids = web_scraping.get_list_of_departments()
+
+        # Se não for possível obter os códigos dos departamentos, exibe uma mensagem de erro
+        if departments_ids is None:
+            self.display_error_message("department_ids")
+            return
+
+        # Atualiza as disciplinas do período atual
+        start_time = time()
+        self.update_departments(departments_ids=departments_ids, year=current_year, period=current_period)
+        self.display_success_message(operation="atualização de departamentos e matérias do período atual", start_time=start_time)
+
+        # Atualiza as disciplinas do período seguinte
+        start_time = time()
+        self.update_departments(departments_ids=departments_ids, year=next_period_year, period=next_period)
+        self.display_success_message(operation="atualização de departamentos e matérias", start_time=start_time)
+
+    def update_departments(self, departments_ids: list, year: str, period: str) -> None:
+        for department_id in departments_ids:
+            disciplines_list = web_scraping.get_department_disciplines(department_id=department_id, current_year=year, current_period=period)
+            department = db_handler.create_department(code=department_id, year=year, period=period)
+
+            # Para cada disciplina do período atual, deleta as turmas previamente cadastradas e cadastra novas turmas no banco de dados
+            for discipline_code in disciplines_list:
+                classes_info = disciplines_list[discipline_code]
+
+                # Cria ou pega a disciplina
+                discipline = db_handler.create_discipline(name=classes_info[0]["name"], code=discipline_code, department=department)
+                # Deleta as turmas previamente cadastradas
+                db_handler.delete_classes_from_discipline(discipline=discipline)
+
+                # Cadastra as novas turmas
+                for class_info in classes_info:
+                    db_handler.create_class(workload=class_info["workload"], teachers=class_info["teachers"],
+                                            classroom=class_info["classroom"], schedule=class_info["schedule"],
+                                            days=class_info["days"], _class=class_info["class_code"], discipline=discipline)
 
     def display_error_message(self, operation: str) -> None:
         print("Não foi possível realizar a operação de atualização do banco de dados.")
         print("Verifique se o SIGAA está funcionando corretamente.")
         print(f"Falha em {operation}")
+
+    def display_success_message(self, operation: str, start_time: float) -> None:
+        print("Operação de atualização do banco de dados realizada com sucesso.")
+        print(f"Sucesso em {operation}")
+        print(f"Tempo de execução: {(time() - start_time):.1f}s")
