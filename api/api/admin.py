cb650a2f35bb0f614133948481ebda0657513cd6
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-06 14:34:41 -0300

    api(admin): adiciona e config as models a página

--- api/api/admin.py
+++ api/api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Department, Discipline, Class
 
-# Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['code', 'year', 'period']
+    search_fields = ['code']
+    ordering = ['year', 'period']
+
+
+@admin.register(Discipline)
+class DisciplineAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code']
+    search_fields = ['name', 'code']
+    ordering = ['name']
+
+
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ['discipline', 'classroom', 'schedule']
+    search_fields = ['discipline__name']
+    ordering = ['discipline__name']
