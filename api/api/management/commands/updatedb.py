f71776974b6efaad6de52798fce2804932a86def
GabrielCastelo-31 <gabflubr@outlook.com>
2023-11-05 22:55:57 -0300

    Updating command line class updatedb. Co-authored-by: Caio Felipe <caiofeliperro@gmail.com>

--- api/api/management/commands/updatedb.py
+++ api/api/management/commands/updatedb.py
@@ -7,15 +7,18 @@ from decouple import config
 """Comando para atualizar o banco de dados."""
 class Command(BaseCommand):
     help = "Atualiza o banco de dados com as disciplinas do SIGAA e suas respectivas turmas."
+
     def handle(self, *args: Any, **options: Any):
-        departments_ids = web_scraping.get_list_of_departments()
+        departments_ids = web_scraping.get_list_of_departments() #Obtem os códigos dos departamentos
 
         if departments_ids is None:
-            self.display_error_message("department_ids")
+            self.display_error_message("department_ids") #Exibe uma mensagem de erro caso não seja possível obter os códigos dos departamentos
             return
 
+        # Obtem o ano e o período atual e o ano e o período seguinte
         current_year, current_period = sessions.get_current_year_and_period()
-        next_year, next_period = sessions.get_next_period()
+        next_period_year, next_period = sessions.get_next_period()
+
 
     def display_error_message(self, operation: str) -> None:
         print("Não foi possível realizar a operação de atualização do banco de dados.")
