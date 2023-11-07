41cca790543c397802704e46fbf4a4805b959e41
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-06 21:14:50 -0300

    docs(test): descreve cada teste feito no app users
    Co-authored-by: HenrikhKenino <110349110+HenrikhKenino@users.noreply.github.com>

--- api/users/tests.py
+++ api/users/tests.py
@@ -4,6 +4,7 @@ from rest_framework.test import APITestCase
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from users.models import User
 from http.cookies import SimpleCookie
+from django.http import HttpResponse
 from datetime import timedelta
 from decouple import config
 import time
@@ -11,8 +12,29 @@ import jwt
 
 
 class UserSessionRegisterTests(APITestCase):
+    """
+    Classe específica para testar a rota de registro de usuários.
 
-    def make_register_post_request(self, access_token=None, provider=None):
+    ```
+    from users.views import Register
+    ```
+    """
+
+    def make_register_post_request(self, access_token: str | None = None, provider: str | None = None) -> HttpResponse:
+        """
+        Método para fazer uma requisição POST para a rota de registro de usuários.
+
+        Args:
+            access_token (str | None): Token de acesso do usuário.
+            provider (str | None): Provedor de autenticação do usuário.
+
+        Returns:
+            response (HttpResponse): Resposta do servidor.
+
+        Observações:
+            - Se o token de acesso for None, será usado um token mock.
+        """
+
         if access_token == None:
             access_token = config('GOOGLE_OAUTH2_MOCK_TOKEN')
 
@@ -23,7 +45,15 @@ class UserSessionRegisterTests(APITestCase):
 
         return self.client.post(url, info, format='json')
 
-    def test_google_register_with_invalid_token(self):
+    def test_google_register_with_invalid_token(self) -> None:
+        """
+        Testa o registro de um usuário com um token inválido.
+
+        Testes:
+            - Mensagem de erro.
+            - Status code (400 BAD REQUEST).
+        """
+
         response = self.make_register_post_request(
             access_token='wrong_token',
             provider='google'
@@ -32,7 +62,15 @@ class UserSessionRegisterTests(APITestCase):
         self.assertEqual(response.data.get('errors'), 'Invalid token')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_google_register_with_empty_token(self):
+    def test_google_register_with_empty_token(self) -> None:
+        """
+        Testa o registro de um usuário com um token vazio.
+
+        Testes:
+            - Mensagem de erro.
+            - Status code (400 BAD REQUEST).
+        """
+
         response = self.make_register_post_request(
             access_token='',
             provider='google'
@@ -41,7 +79,18 @@ class UserSessionRegisterTests(APITestCase):
         self.assertEqual(response.data.get('errors'), 'Invalid token')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_google_register_with_valid_token(self):
+    def test_google_register_with_valid_token(self) -> None:
+        """
+        Testa o registro de um usuário com um token válido.
+
+        Testes:
+            - Usuário criado ou acessado.
+            - Nome do usuário.
+            - Sobrenome do usuário.
+            - E-mail do usuário.
+            - Status code (200 OK).
+        """
+
         response = self.make_register_post_request(
             provider='google'
         )
@@ -55,7 +104,15 @@ class UserSessionRegisterTests(APITestCase):
         self.assertEqual(created_user.email, 'user@email.com')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_register_with_invalid_provider(self):
+    def test_register_with_invalid_provider(self) -> None:
+        """
+        Testa o registro de um usuário com um provedor inválido.
+
+        Testes:
+            - Mensagem de erro.
+            - Status code (400 BAD REQUEST).
+        """
+
         provider = 'wrong_provider'
         response = self.make_register_post_request(
             access_token='token',
@@ -66,7 +123,18 @@ class UserSessionRegisterTests(APITestCase):
         self.assertEqual(erros, f'Invalid provider {provider}')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_google_register_refresh_cookie_when_valid_token(self):
+    def test_google_register_refresh_cookie_when_valid_token(self) -> None:
+        """
+        Testa o refresh cookie quando o token de registro de usuário é válido.
+
+        Testes:
+            - Refresh cookie.
+            - Tempo de vida do cookie.
+            - HttpOnly.
+            - Secure.
+            - Samesite.
+        """
+
         response = self.make_register_post_request(
             provider='google'
         )
@@ -79,7 +147,15 @@ class UserSessionRegisterTests(APITestCase):
         self.assertTrue(refresh_cookie.get('secure'))
         self.assertEqual(refresh_cookie.get('samesite'), "Lax")
 
-    def test_google_register_refresh_cookie_when_invalid_token(self):
+    def test_google_register_refresh_cookie_when_invalid_token(self) -> None:
+        """
+        Testa o refresh cookie quando o token de registro de usuário é inválido.
+
+        Testes:
+            - Refresh cookie não existe.
+            - Refresh cookie não está no corpo da resposta.
+        """
+
         response = self.make_register_post_request(
             access_token='wrong_token',
             provider='google'
@@ -90,8 +166,19 @@ class UserSessionRegisterTests(APITestCase):
 
 
 class UserSessionLoginTests(APITestCase):
+    """
+    Classe específica para testar a rota de login de usuários.
 
-    def setUp(self):
+    ```
+    from users.views import RefreshJWTView
+    ```
+    """
+
+    def setUp(self) -> None:
+        """
+        Método para criar um usuário para os testes.
+        """
+
         self.user, _ = User.objects.get_or_create(
             first_name="test",
             last_name="banana",
@@ -99,7 +186,19 @@ class UserSessionLoginTests(APITestCase):
         )
         self.user.save()
 
-    def make_login_post_request(self, cookie_enable=True, cookie_expired=False, cookie_value=None):
+    def make_login_post_request(self, cookie_enable: bool = True, cookie_expired: bool = False, cookie_value: str | None = None) -> HttpResponse:
+        """
+        Método para fazer uma requisição POST para a rota de login de usuários.
+
+        Args:
+            cookie_enable (bool): Habilita o uso de cookies.
+            cookie_expired (bool): Habilita o cookie expirado.
+            cookie_value (str | None): Valor do cookie.
+        
+        Returns:
+            response (HttpResponse): Resposta do servidor.
+        """
+
         if cookie_enable:
             refresh_token = TokenObtainPairSerializer.get_token(self.user)
 
@@ -113,12 +212,29 @@ class UserSessionLoginTests(APITestCase):
         url = reverse('users:login')
         return self.client.post(url, {}, format='json')
 
-    def test_user_login_with_invalid_token(self):
+    def test_user_login_with_invalid_token(self) -> None:
+        """
+        Testa o login de um usuário com um token inválido.
+
+        Testes:
+            - Status code (401 UNAUTHORIZED).
+        """
+
         response = self.make_login_post_request(cookie_value='wrong_token')
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_user_login_with_valid_token(self):
+    def test_user_login_with_valid_token(self) -> None:
+        """
+        Testa o login de um usuário com um token válido.
+
+        Testes:
+            - Status code (200 OK).
+            - Nome do usuário.
+            - Sobrenome do usuário.
+            - E-mail do usuário.
+        """
+
         response = self.make_login_post_request()
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -126,7 +242,15 @@ class UserSessionLoginTests(APITestCase):
         self.assertEqual(response.data['last_name'], self.user.last_name)
         self.assertEqual(response.data['email'], self.user.email)
 
-    def test_user_login_access_token(self):
+    def test_user_login_access_token(self) -> None:
+        """
+        Testa access token do usuário com um token válido.
+
+        Testes:
+            - Access token no corpo da resposta.
+            - Tempo de vida do access token.
+        """
+
         response = self.make_login_post_request()
 
         access_token = response.data.get('access')
@@ -151,14 +275,32 @@ class UserSessionLoginTests(APITestCase):
         self.assertTrue(check_in_range(decoded_token))
         self.assertTrue('access' in response.data)
 
-    def test_user_login_without_refresh_token(self):
+    def test_user_login_without_refresh_token(self) -> None:
+        """
+        Testa o login de um usuário sem o refresh token.
+
+        Testes:
+            - Código de erro.
+            - Mensagem de erro.
+            - Status code (401 UNAUTHORIZED).
+        """
+
         response = self.make_login_post_request(cookie_enable=False)
 
         self.assertEqual(response.data.get('detail').code, 'not_authenticated')
         self.assertEqual(response.data.get('detail'), 'Refresh cookie error.')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_user_login_with_expired_cookie(self):
+    def test_user_login_with_expired_cookie(self) -> None:
+        """
+        Testa o login de um usuário com um cookie expirado.
+
+        Testes:
+            - Código de erro.
+            - Mensagem de erro.
+            - Status code (401 UNAUTHORIZED).
+        """
+
         response = self.make_login_post_request(cookie_expired=True)
 
         self.assertEqual(response.data.get('code'), 'token_not_valid')
