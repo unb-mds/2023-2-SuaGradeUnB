0dbc6533643615214ab66a0f77b06e27116a1fcd
MateusVrs <mateusvrs.dev@gmail.com>
2023-11-07 10:46:05 -0300

    api(urls): refatora a adição de rota p/tests

--- api/utils/urls.py
+++ api/utils/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 from .views import mocked_departments
-import sys
 
 app_name = 'utils'
 
-urlpatterns = []
-
-if 'test' in sys.argv:
-    urlpatterns += [
-        path('departments/', mocked_departments, name='departments'),
-    ]
+urlpatterns = [
+    path('mock/departments/', mocked_departments, name='departments'),
+]
