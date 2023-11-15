76508212f9b82e0876e53dbebb16b4e4379a9248
Arthur Ribeiro <artrsousa1@gmail.com>
2023-11-15 18:55:09 -0300

    frontend(paths):criação das rotas das páginas

---
+++ web/app/schedules/mygrades/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import useUser from '@/app/hooks/useUser';
+
+export default function MyGrades() {
+    const { user } = useUser();
+
+    return (
+        <></>
+    );
+}
---
+++ web/app/schedules/profile/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import useUser from '@/app/hooks/useUser';
+
+export default function Profile() {
+    const { user } = useUser();
+
+    return (
+        <></>
+    );
+}
