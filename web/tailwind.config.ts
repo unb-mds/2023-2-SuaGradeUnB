cdc84ed162276638a29d9566dd24ddd45bd9006d
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-15 13:27:33 -0300

    package(node): update package.json + tailwind

---
+++ web/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        sans: ['var(--font-poppins)'],
+      },
+      colors: {
+        primary: '#0F8548'
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
