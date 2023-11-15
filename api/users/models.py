8bff743e7c85180c9d06d2de6bdd1ecffea7930a
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-15 19:14:10 -0300

    users(model): add picture url field to User mode

--- api/users/models.py
+++ api/users/models.py
@@ -5,9 +5,10 @@ import uuid
 
 class User(AbstractUser):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=128)
+    first_name = models.CharField(default='', max_length=64)
+    last_name = models.CharField(default='', max_length=128)
     email = models.EmailField(blank=False, unique=True)
+    picture_url = models.URLField(default='')
     is_active = models.BooleanField(default=True)
 
     REQUIRED_FIELDS = ["email"]
