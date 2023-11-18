129b463572d15f5b7cb12fedf89c46b5a8a66a59
Caio <caiofhc02@gmail.com>
2023-11-18 17:12:19 -0300

    comecando o Button `adicionar materia`

--- web/app/schedules/home/page.tsx
+++ web/app/schedules/home/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Link from 'next/link';
 
 import useUser from '@/app/hooks/useUser';
 
@@ -6,6 +7,12 @@ export default function Home() {
     const { user } = useUser();
 
     return (
-        <></>
+    <main>
+        <div className='flex flex-col justify-center items-center'>
+        <Link href={''} className='flex flex-col items-center font-medium rounded w-2/4 py-2 px-1 shadow-2xl hover:shadow-md transition-all duration-300 bg-[#4080F4] text-white'>
+        Adicionar materia</Link>
+        </div>
+
+    </main>
     );
 }
