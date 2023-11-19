2cd6a3ad8b6246ea6b2981ded6e9589728746a68
Caio <caiofhc02@gmail.com>
2023-11-19 20:27:45 -0300

    Typo em rounded no botao

--- web/app/schedules/home/page.tsx
+++ web/app/schedules/home/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     return (
     <main>
         <div className='flex flex-col justify-center items-center'>
-        <Link href={''} className='flex flex-col items-center font-medium rounded w-2/4 py-2 px-1 shadow-2xl hover:shadow-md transition-all duration-300 bg-[#4080F4] text-white'>
+        <Link href={''} className='flex flex-col items-center font-medium rounded-lg w-2/4 py-2 px-1 shadow-2xl hover:shadow-md transition-all duration-300 bg-[#4080F4] text-white'>
         Adicionar materia</Link>
         </div>
 
