eb158d3d820812567e0fd997491644b7eb5fcdbf
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-22 00:33:31 -0300

    users(urls): add logout path access

--- api/users/urls.py
+++ api/users/urls.py
@@ -6,4 +6,5 @@ app_name = 'users'
 urlpatterns = [
     re_path('register/' + r'(?P<oauth2>[^/]+)/$', views.Register.as_view(), name='register'),
     path('login/', views.RefreshJWTView.as_view(), name='login'),
+    path('logout/', views.BlacklistJWTView.as_view(), name='logout'),
 ]
