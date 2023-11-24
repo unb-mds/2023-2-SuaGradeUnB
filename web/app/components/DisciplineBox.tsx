9868db9d245bed2688d0139d759c56b31650cb00
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-24 01:50:33 -0300

    web(DisciplineBox): adiciona trash icon

--- web/app/components/DisciplineBox.tsx
+++ web/app/components/DisciplineBox.tsx
@@ -7,14 +7,21 @@ interface DisciplineBoxPropsType {
 
 export default function DisciplineBox(props: DisciplineBoxPropsType) {
     return (
-        <span className='flex flex-col justify-center gap-1 rounded-lg font-semibold w-10/12 max-h-28 py-2 px-4 mx-4 shadow-lg hover:shadow-md bg-slate-200/100 text-sm'>
-            <section>
-                {props.name} - {props.code}
-            </section>
-            <section>
-                {props.teachers[0]} {props.teachers.length > 1 ? 'e outros' : ''}
-            </section>
-            <section>{props.schedule}</section>
+        <span className='flex items-center justify-between rounded-lg font-semibold w-10/12 max-h-28 py-2 px-4 shadow-lg bg-snow-primary text-sm'>
+            <div className='flex flex-col gap-1'>
+                <section>
+                    {props.name} - {props.code}
+                </section>
+                {props.teachers.length && (
+                    <section>
+                        {props.teachers[0]} {props.teachers.length > 1 && 'e outros'}
+                    </section>
+                )}
+                <section>{props.schedule}</section>
+            </div>
+            <button className='material-symbols-rounded hover:text-red-900'>
+                delete
+            </button>
         </span>
     );
 }
