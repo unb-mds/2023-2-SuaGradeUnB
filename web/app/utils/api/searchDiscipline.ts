09a28d0505cce0ff8e3f83ed735e8a6b6e7ea5ca
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-24 01:51:03 -0300

    web(api): função para integrar busca de disciplina

---
+++ web/app/utils/api/searchDiscipline.ts
@@ -0,0 +1,37 @@
+import toast from 'react-hot-toast';
+
+import request from '../request';
+import { settings } from '../settings';
+
+export type ResponseType = Array<{
+    code: string,
+    name: string,
+    classes: Array<{
+        _class: string,
+        classroom: string,
+        days: Array<string>,
+        schedule: string,
+        teachers: Array<string>
+    }>
+}>
+
+export default async function searchDiscipline(search: string, year: string, period: string) {
+    const params = {
+        search: search,
+        year: year,
+        period: period
+    };
+
+    try {
+        const response = await request.get('/courses/', {
+            ...settings,
+            params: params
+        });
+
+        const data: ResponseType = response.data;
+        console.log(data);
+        return data;
+    } catch (error: any) {
+        toast.error(error.response.data.detail);
+    }
+} 
