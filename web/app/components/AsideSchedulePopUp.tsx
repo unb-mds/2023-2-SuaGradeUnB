02520a68ac7efe6872cffc3cc975718f48ccb5f0
mateusvrs <mateusvrs.dev@gmail.com>
2023-11-24 17:03:03 -0300

    web(home): cria secção de expandir p/cada matéria

--- web/app/components/AsideSchedulePopUp.tsx
+++ web/app/components/AsideSchedulePopUp.tsx
@@ -1,8 +1,12 @@
 import { Dispatch, LegacyRef, SetStateAction, useState } from 'react';
 
 import Button from './Button';
+
 import searchDiscipline, { ResponseType } from '../utils/api/searchDiscipline';
+
 import toast from 'react-hot-toast';
+
+import styles from '@/app/components/styles/tooltip.module.css';
 
 interface AsideSchedulePopUpPropsType {
     content: {
@@ -57,7 +61,16 @@ export default function AsideSchedulePopUp(props: AsideSchedulePopUpPropsType) {
                                             else if (!year || !period) toast.error('Escolha o ano/período');
                                             else {
                                                 const data = await searchDiscipline(textSearch, year, period);
-                                                if (data) setSearchedDisciplineInfos(data);
+                                                if (data) {
+                                                    let infos: ResponseType[0][] = [];
+                                                    data.forEach(discipline => {
+                                                        infos.push({
+                                                            ...discipline,
+                                                            expanded: false
+                                                        });
+                                                    });
+                                                    setSearchedDisciplineInfos(infos);
+                                                }
                                             }
                                         }}
                                         className='material-symbols-rounded'
@@ -69,7 +82,7 @@ export default function AsideSchedulePopUp(props: AsideSchedulePopUpPropsType) {
                             <div className='flex flex-col items-center gap-1'>
                                 <span className='text-xl font-semibold'>Ano/Período</span>
                                 <select
-                                    className='bg-white h-14 w-80 p-2 rounded-xl'
+                                    className='bg-white shadow-md h-14 w-80 p-2 rounded-xl'
                                     value={`${formData.year}/${formData.period}`}
                                     onChange={(e) => {
                                         const text = e.target.value.trim();
@@ -92,21 +105,59 @@ export default function AsideSchedulePopUp(props: AsideSchedulePopUpPropsType) {
                                     <option value="2024/1">2024/1</option>
                                 </select>
                             </div>
-                            <div className='flex flex-col gap-5 overflow-auto max-h-full w-10/12 mx-5'>
-                                {searchedDisciplineInfos.map((discipline) => (
-                                    <div className='flex flex-col gap-5' key={discipline.code}>
-                                        <span className='font-semibold'>{discipline.name} - {discipline.code}</span>
-                                        {discipline.classes.map((cls, index) => (
-                                            <div className='flex flex-col' key={index}>
-                                                <div>
-                                                    <span className='font-semibold'>Sala:</span> {cls.classroom}
-                                                </div>
-                                                <div className='flex gap-2'>
-                                                    <span className='font-semibold'>Horários:</span> {cls.schedule}
-                                                </div>
+                            <div className='flex flex-col gap-2 w-10/12 h-[50%] overflow-auto text-lg'>
+                                {searchedDisciplineInfos.map((discipline, index) => (
+                                    <>
+                                        <button
+                                            onClick={() => {
+                                                let newInfo: ResponseType[0][] = [];
+                                                searchedDisciplineInfos.forEach((discipline, number) => {
+                                                    if (number != index) newInfo.push(discipline);
+                                                    else {
+                                                        newInfo.push({
+                                                            ...discipline,
+                                                            expanded: !discipline.expanded
+                                                        });
+                                                    }
+                                                });
+
+                                                setSearchedDisciplineInfos(newInfo);
+                                            }}
+                                            className='flex items-center gap-3'
+                                        >
+                                            <span className="material-symbols-rounded">
+                                                {discipline.expanded ? 'expand_less' : 'expand_more'}
+                                            </span>
+                                            <span className='font-semibold'>{discipline.name} - {discipline.code}</span>
+                                        </button>
+                                        {discipline.expanded &&
+                                            <div className='overflow-auto min-h-full'>
+                                                {discipline.classes.map((cls, index) => (
+                                                    <div className='flex flex-col hover:bg-gray-300 hover:bg-opacity-40 rounded-md p-2' key={index}>
+                                                        <div>
+                                                            <span className='font-semibold'>Sala:</span> {cls.classroom}
+                                                        </div>
+                                                        <div className='flex gap-1'>
+                                                            <span className='font-semibold'>Horários:</span> {cls.schedule}
+                                                            <div className={styles.tooltip}>
+                                                                <span className='material-symbols-rounded text-xs'>question_mark</span>
+                                                                <span className={`${styles.tooltiptext} p-2`}>
+                                                                    {cls.days.map((day, index) =>
+                                                                        <div key={index}>
+                                                                            {day}
+                                                                        </div>
+                                                                    )}
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                        <div>
+                                                            <span className='font-semibold'>Professor:</span> {cls.teachers[0]} {cls.teachers.length > 1 && 'e outros'}
+                                                        </div>
+                                                    </div>
+                                                ))}
                                             </div>
-                                        ))}
-                                    </div>
+                                        }
+                                    </>
                                 ))}
                             </div>
                         </section>
