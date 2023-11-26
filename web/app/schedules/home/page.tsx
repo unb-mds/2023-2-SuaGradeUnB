9877f3865ada2885d87d09dd4d0efb363bb73079
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-26 20:27:37 -0300

    web(home): aparição das disciplinas selecionadas

--- web/app/schedules/home/page.tsx
+++ web/app/schedules/home/page.tsx
@@ -2,14 +2,19 @@
 
 import { useEffect, useRef, useState } from 'react';
 import { useRouter } from 'next/navigation';
+import useSelectedClasses from '@/app/hooks/useSelectedClasses';
+
+import { ClassValueType } from '@/app/contexts/SelectedClassesContext';
 
 import Button from '@/app/components/Button';
 import DisciplineBox from '@/app/components/DisciplineBox';
-import AsideSchedulePopUp from '@/app/components/AsideSchedulePopUp';
+import AsideSchedulePopUp from '@/app/components/AsideSchedulePopUp/AsideSchedulePopUp';
 
 export default function Home() {
    const router = useRouter();
 
+   const [classesToShow, setClassesToShow] = useState<Array<ClassValueType>>([]);
+   const { classesChange, selectedClasses } = useSelectedClasses();
    const [showContent, setShowContent] = useState(false);
 
    const divAddClassRef = useRef(null);
@@ -33,6 +38,16 @@ export default function Home() {
       };
    }, []);
 
+   useEffect(() => {
+      const newClasses = new Array();
+      selectedClasses.forEach((cls) => {
+         cls.forEach(value => {
+            newClasses.push(value);
+         });
+      });
+      setClassesToShow(newClasses);
+   }, [classesChange, selectedClasses]);
+
    return (
       <>
          <div className='flex justify-center'>
@@ -45,23 +60,22 @@ export default function Home() {
             </Button>
          </div>
 
-         <div className='flex flex-col items-center gap-5 overflow-auto max-h-full pb-2'>
-            <DisciplineBox name='Cálculo 1' code='MAT0025' teachers={['Ricardo']} schedule='Segunda, Terça - Tal Horário' />
-            <DisciplineBox name='Cálculo 1' code='MAT0025' teachers={['Ricardo', 'dois', 'três']} schedule='Segunda, Terça - Tal Horário' />
-            <DisciplineBox name='Cálculo 1' code='MAT0025' teachers={['Ricardo', 'dois', 'três']} schedule='Segunda, Terça - Tal Horário' />
-            <DisciplineBox name='Cálculo 1' code='MAT0025' teachers={['Ricardo', 'dois', 'três']} schedule='Segunda, Terça - Tal Horário' />
-            <DisciplineBox name='Cálculo 1' code='MAT0025' teachers={['Ricardo', 'dois', 'três']} schedule='Segunda, Terça - Tal Horário' />
-            <DisciplineBox name='Cálculo 1' code='MAT0025' teachers={['Ricardo', 'dois', 'três']} schedule='Segunda, Terça - Tal Horário' />
-            <DisciplineBox name='Cálculo 1' code='MAT0025' teachers={['Ricardo', 'dois', 'três']} schedule='Segunda, Terça - Tal Horário' />
-            <DisciplineBox name='Cálculo 1' code='MAT0025' teachers={['Ricardo', 'dois', 'três']} schedule='Segunda, Terça - Tal Horário' />
+         <div className={`flex flex-col items-center ${classesToShow.length ? '' : 'justify-center'} gap-5 overflow-auto min-h-full max-h-full pb-2`}>
+            {classesToShow.length ? classesToShow.map((cls, index) =>
+               <DisciplineBox
+                  key={index}
+                  currentClass={cls} discipline={cls.discipline}
+               />
+            ) : 'Nenhuma disciplina escolhida no momento'}
          </div>
 
          <AsideSchedulePopUp content={{ showContent, setShowContent }} divAddClassRef={divAddClassRef} />
 
          <div className='flex justify-center'>
             <Button
+               disabled={classesToShow.length === 0}
                onClick={() => router.replace('/schedules/mygrades')}
-               className='absolute bottom-32 w-52 h-10 font-semibold bg-primary'
+               className='absolute bottom-28 w-52 h-10 font-semibold bg-primary disabled:opacity-70'
             >
                Gerar Grade
             </Button>
