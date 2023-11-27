cdab9583bcb0bab637fdda46a2929973417e9135
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-27 12:05:58 -0300

    api(views): add rota de ano/período disponíveis
    Co-authored-by: GabrielCastelo-31 <gabflubr@outlook.com>

--- api/api/urls.py
+++ api/api/urls.py
@@ -4,5 +4,6 @@ from api import views
 app_name = 'api'
 
 urlpatterns = [
-    path('', views.Search.as_view(), name="search")
+    path('', views.Search.as_view(), name="search"),
+    path('year-period/', views.YearPeriod.as_view(), name="year-period")
 ]
--- api/api/views.py
+++ api/api/views.py
@@ -1,6 +1,7 @@
 from utils.db_handler import filter_disciplines_by_name, filter_disciplines_by_code, filter_disciplines_by_year_and_period
 from rest_framework.decorators import APIView
 from .serializers import DisciplineSerializer
+from utils.sessions import get_current_year_and_period, get_next_period
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
@@ -15,7 +16,7 @@ class Search(APIView):
             string = string.strip()
 
         return string
-    
+
     def get(self, request: Request, *args, **kwargs) -> Response:
         name = self.treat_string(request.GET.get('search', None))
         year = self.treat_string(request.GET.get('year', None))
@@ -38,9 +39,21 @@ class Search(APIView):
 
             for term in name[1:]:
                 disciplines &= filter_disciplines_by_code(code=term)
-            
+
         filtered_disciplines = filter_disciplines_by_year_and_period(
             year=year, period=period, disciplines=disciplines)
         data = DisciplineSerializer(filtered_disciplines, many=True).data
 
         return Response(data[:MAXIMUM_RETURNED_DISCIPLINES], status.HTTP_200_OK)
+
+
+class YearPeriod(APIView):
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        year, period = get_current_year_and_period()
+        next_year, next_period = get_next_period()
+
+        data = {
+            'year/period': [f'{year}/{period}', f'{next_year}/{next_period}'],
+        }
+
+        return Response(data, status.HTTP_200_OK)
