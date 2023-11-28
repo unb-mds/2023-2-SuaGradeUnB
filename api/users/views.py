d5503f9fcbd631584eb462881631c1d11483efd3
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-28 01:50:29 -0300

    docs(users): descreve view de registro c/swagger

--- api/users/views.py
+++ api/users/views.py
@@ -5,10 +5,66 @@ from rest_framework.request import Request
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenBlacklistView
 from users.backends.utils import get_backend
 from users.simplejwt.decorators import move_refresh_token_to_cookie
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 
 class Register(TokenObtainPairView):
 
+    @swagger_auto_schema(
+        operation_description="""Registra um novo usuário no sistema, ou retorna os dados caso o mesmo já exista.
+        O header da resposta acompanha o token de `refresh` nos cookies no seguinte formato:
+
+        headers = {
+            "Set-Cookie": "refresh=<refresh-token>; Secure; HttpOnly; SameSite=Lax; Expires=<expires-date>"
+        }
+        """,
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'access_token': openapi.Schema(
+                    type=openapi.TYPE_STRING,
+                    description='Token de acesso provido pelo provedor \n de autenticação (Google, Facebook, etc)'
+                ),
+            }
+        ),
+        responses={
+            200: openapi.Response('OK', openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'access': openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        description='Token de acesso JWT'
+                    ),
+                    'first_name': openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        description='Primeiro nome do usuário'
+                    ),
+                    'last_name': openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        description='Último nome do usuário'
+                    ),
+                    'picture_url': openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        description='URL da foto do usuário'
+                    ),
+                    'email': openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        description='Email do usuário'
+                    ),
+                }
+            )),
+            400: openapi.Response('Bad Request', openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'errors': openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        description='Mensagem de erro'
+                    ),
+                }
+            )),
+        }
+    )
     @move_refresh_token_to_cookie
     def post(self, request: Request, *args, **kwargs) -> Response:
         token = request.data.get('access_token')
