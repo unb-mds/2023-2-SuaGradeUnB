0d0e63d22439d31c260ea77032679b5d764074ad
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-28 01:51:13 -0300

    redoc(api): descreve views de search e year/period

--- api/api/views.py
+++ api/api/views.py
@@ -1,54 +1,100 @@
-from utils.db_handler import filter_disciplines_by_name, filter_disciplines_by_code, filter_disciplines_by_year_and_period
+from utils import db_handler as dbh
 from rest_framework.decorators import APIView
-from .serializers import DisciplineSerializer
 from utils.sessions import get_current_year_and_period, get_next_period
-from rest_framework.response import Response
-from rest_framework.request import Request
-from rest_framework import status
+from rest_framework import status, request, response
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from api import serializers
 
 MAXIMUM_RETURNED_DISCIPLINES = 5
 ERROR_MESSAGE = "no valid argument found for 'search', 'year' or 'period'"
 
 
 class Search(APIView):
+
     def treat_string(self, string: str | None) -> str | None:
         if string is not None:
             string = string.strip()
 
         return string
 
-    def get(self, request: Request, *args, **kwargs) -> Response:
+    @swagger_auto_schema(
+        operation_description="Busca disciplinas por nome ou código. O ano e período são obrigatórios.",
+        manual_parameters=[
+            openapi.Parameter('search', openapi.IN_QUERY,
+                              description="Termo de pesquisa (Nome/Código)", type=openapi.TYPE_STRING),
+            openapi.Parameter('year', openapi.IN_QUERY,
+                              description="Ano", type=openapi.TYPE_INTEGER),
+            openapi.Parameter('period', openapi.IN_QUERY,
+                              description="Período  ", type=openapi.TYPE_INTEGER),
+        ],
+        responses={
+            200: openapi.Response('OK', serializers.DisciplineSerializer),
+            400: openapi.Response('BAD_REQUEST', openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'errors': openapi.Schema(
+                        type=openapi.TYPE_STRING,
+                        description='Mensagem de erro'
+                    ),
+                }
+            )),
+        }
+    )
+    def get(self, request: request.Request, *args, **kwargs) -> response.Response:
         name = self.treat_string(request.GET.get('search', None))
         year = self.treat_string(request.GET.get('year', None))
         period = self.treat_string(request.GET.get('period', None))
 
         if name is None or len(name) == 0 or year is None or len(year) == 0 or period is None or len(period) == 0:
-            return Response(
+            return response.Response(
                 {
                     "errors": ERROR_MESSAGE
                 }, status.HTTP_400_BAD_REQUEST)
 
         name = name.split()
-        disciplines = filter_disciplines_by_name(name=name[0])
+        disciplines = dbh.filter_disciplines_by_name(name=name[0])
 
         for term in name[1:]:
-            disciplines &= filter_disciplines_by_name(name=term)
+            disciplines &= dbh.filter_disciplines_by_name(name=term)
 
         if not disciplines.count():
-            disciplines = filter_disciplines_by_code(code=name[0])
+            disciplines = dbh.filter_disciplines_by_code(code=name[0])
 
             for term in name[1:]:
-                disciplines &= filter_disciplines_by_code(code=term)
+                disciplines &= dbh.filter_disciplines_by_code(code=term)
 
-        filtered_disciplines = filter_disciplines_by_year_and_period(
+        filtered_disciplines = dbh.filter_disciplines_by_year_and_period(
             year=year, period=period, disciplines=disciplines)
-        data = DisciplineSerializer(filtered_disciplines, many=True).data
+        data = serializers.DisciplineSerializer(
+            filtered_disciplines, many=True).data
 
-        return Response(data[:MAXIMUM_RETURNED_DISCIPLINES], status.HTTP_200_OK)
+        return response.Response(data[:MAXIMUM_RETURNED_DISCIPLINES], status.HTTP_200_OK)
 
 
 class YearPeriod(APIView):
-    def get(self, request: Request, *args, **kwargs) -> Response:
+
+    @swagger_auto_schema(
+        operation_description="Retorna o ano e período atual, e o próximo ano e período letivos válidos para pesquisa.",
+        responses={
+            200: openapi.Response('OK', openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    'year/period': openapi.Schema(
+                        type=openapi.TYPE_ARRAY,
+                        items=openapi.Schema(
+                            type=openapi.TYPE_STRING
+                        )
+                    )
+                }
+            ), examples={
+                'application/json': {
+                    'year/period': ['2020/1', '2020/2']
+                }
+            })
+        }
+    )
+    def get(self, request: request.Request, *args, **kwargs) -> response.Response:
         year, period = get_current_year_and_period()
         next_year, next_period = get_next_period()
 
@@ -56,4 +102,4 @@ class YearPeriod(APIView):
             'year/period': [f'{year}/{period}', f'{next_year}/{next_period}'],
         }
 
-        return Response(data, status.HTTP_200_OK)
+        return response.Response(data, status.HTTP_200_OK)
