cb62aa61538ea5331c708d72aac490da7c601f21
mateusvrs <mateusvrs.dev@gmail.com>
2023-11-28 12:35:07 -0300

    docs(api): simplifica classe de erro swagger

--- api/api/swagger.py
+++ api/api/swagger.py
@@ -18,20 +18,23 @@ class Errors():
             }
         )
 
+    def add_error(self, swagger_erros: dict[openapi.Response], error: int, key_error: KeyError):
+        check_error = str(error).startswith('4') or str(error).startswith('5')
+        if check_error:
+            message = status_codes._codes[error][0].upper()
+            swagger_erros[error] = openapi.Response(message, self.get_schema())
+        else:
+            raise key_error
+
     def retrieve_erros(self) -> dict[openapi.Response]:
         swagger_erros = dict()
 
         for error in self.erros:
+            key_error = KeyError(f'Code {error} is not a valid HTTP error.')
+
             try:
-                if str(error).startswith('4') or str(error).startswith('5'):
-                    message = status_codes._codes[error][0].upper()
-                    swagger_erros[error] = openapi.Response(
-                        message, self.get_schema()
-                    )
-                else:
-                    raise KeyError(f'Code {error} is not a valid HTTP error.')
+                self.add_error(swagger_erros, error, key_error)
             except KeyError:
-                raise KeyError(
-                    f'Code {error} not exists in HTTP status codes.')
+                raise key_error
 
         return swagger_erros
