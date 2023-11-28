08ca3f6762ddda42d57cae2eb052c9dab62b3a43
mateuvrs <mateusvrs.dev@gmail.com>
2023-11-28 01:51:47 -0300

    docs(urls): adiciona rota do redoc e swagger

--- api/core/urls.py
+++ api/core/urls.py
@@ -16,10 +16,32 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 import sys
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sua Grade UnB - API",
+        default_version='v1',
+        description="O Sua Grade UnB é um projeto da matéria Métodos de Desenvolvimento de Software, a qual tem como objetivo auxiliar os alunos da Universidade de Brasília a montarem suas grades horárias de maneira fácil e intuitiva.",
+        contact=openapi.Contact(email="suagradeunb@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Documentation
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
+
+    # Views
     path('users/', include('users.urls')),
     path('courses/', include('api.urls'))
 ]
