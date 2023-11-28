66c7eba9f8da932b9ac6714cb0aeab058932dfa8
Mateus Vieira <68292695+mateusvrs@users.noreply.github.com>
2023-11-28 11:58:38 -0300

    Revert "api(search): corrige experiência do usuário e adiciona verificações"

--- api/api/management/commands/updatedb.py
+++ api/api/management/commands/updatedb.py
@@ -15,8 +15,8 @@ class Command(BaseCommand):
         """Adiciona os argumentos do comando."""
         parser.add_argument('-a', '-all', action='store_true', dest='all', default=False,
                             help="Atualiza o banco de dados com as disciplinas dos períodos atual e seguinte.")
-
-        parser.add_argument('-p', '--period', action='store', default=None,
+             
+        parser.add_argument('-p', '--period', action='store', default=None, 
                             choices=[".".join(sessions.get_current_year_and_period()), ".".join(sessions.get_next_period())],
                             dest='period', help="Atualiza o banco de dados com as disciplinas do período especificado.")
 
@@ -35,19 +35,19 @@ class Command(BaseCommand):
             print("Nenhum período foi especificado.")
             print("Utilize o comando 'updatedb -h' para mais informações.")
             return
-
-        # Obtem o ano e o período anterior ao período atual
+        
+        # Obtem o ano e o período atual e o ano e o período seguinte
         previous_period_year, previous_period = sessions.get_previous_period()
 
-        # Apaga as disciplinas do período anterior
+        # Apaga as disciplinas do período interior
         delete_all_departments_using_year_and_period(
             year=previous_period_year, period=previous_period)
-
+        
         if options["delete"]:
             for year, period in choices:
                 self.delete_period(year=year, period=period)
             return
-
+        
         departments_ids = web_scraping.get_list_of_departments()
 
         if departments_ids is None:
@@ -55,7 +55,7 @@ class Command(BaseCommand):
             return
 
         print("Atualizando o banco de dados...")
-
+        
         for year, period in choices:
             start_time = time()
             self.update_departments(departments_ids=departments_ids, year=year, period=period)
@@ -68,14 +68,14 @@ class Command(BaseCommand):
                 department_id=department_id, current_year=year, current_period=period)
             department = get_or_create_department(
                 code=department_id, year=year, period=period)
-
+            
             # Para cada disciplina do período atual, deleta as turmas previamente cadastradas e cadastra novas turmas no banco de dados
             for discipline_code in disciplines_list:
                 classes_info = disciplines_list[discipline_code]
                 # Cria ou pega a disciplina
                 discipline = get_or_create_discipline(
                     name=classes_info[0]["name"], code=discipline_code, department=department)
-
+                
                 # Deleta as turmas previamente cadastradas
                 delete_classes_from_discipline(discipline=discipline)
 
@@ -104,5 +104,5 @@ class Command(BaseCommand):
 
     def display_success_delete_message(self, operation: str, start_time: float) -> None:
         print("Operação de remoção do banco de dados realizada com sucesso.")
-        print(f"Sucesso em {operation}")
+        print(f"Sucesso em {operation}") 
         print(f"Tempo de execução: {(time() - start_time):.1f}s\n")
