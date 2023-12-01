11d16e5e7be1005e67fa963eb84bc4b79c79e1b0
mateuvrs <mateusvrs.dev@gmail.com>
2023-12-01 08:28:08 -0300

    devops(updatedb): cria melhor descrição de erro

--- api/api/management/commands/updatedb.py
+++ api/api/management/commands/updatedb.py
@@ -17,7 +17,8 @@ class Command(BaseCommand):
                             help="Atualiza o banco de dados com as disciplinas dos períodos atual e seguinte.")
 
         parser.add_argument('-p', '--period', action='store', default=None,
-                            choices=[".".join(sessions.get_current_year_and_period()), ".".join(sessions.get_next_period())],
+                            choices=[".".join(sessions.get_current_year_and_period()), ".".join(
+                                sessions.get_next_period())],
                             dest='period', help="Atualiza o banco de dados com as disciplinas do período especificado.")
 
         parser.add_argument('-d', '--delete', action='store_true', dest='delete', default=False,
@@ -57,18 +58,26 @@ class Command(BaseCommand):
         print("Atualizando o banco de dados...")
 
         for year, period in choices:
-            start_time = time()
-            self.update_departments(departments_ids=departments_ids, year=year, period=period)
-            self.display_success_update_message(operation=f"{year}/{period}", start_time=start_time)
+            try:
+                start_time = time()
+                self.update_departments(
+                    departments_ids=departments_ids, year=year, period=period)
+                self.display_success_update_message(
+                    operation=f"{year}/{period}", start_time=start_time)
+            except Exception as exception:
+                print("Houve um erro na atualização do bando de dados.")
+                print(f"Error: {exception}")
 
     def update_departments(self, departments_ids: list, year: str, period: str) -> None:
         """Atualiza os departamentos do banco de dados e suas respectivas disciplinas."""
         for department_id in departments_ids:
+            print(f"WebScraping do departamento: {department_id}")
             disciplines_list = web_scraping.get_department_disciplines(
                 department_id=department_id, current_year=year, current_period=period)
             department = get_or_create_department(
                 code=department_id, year=year, period=period)
 
+            print("Atualizando disciplinas do departamento...")
             # Para cada disciplina do período atual, deleta as turmas previamente cadastradas e cadastra novas turmas no banco de dados
             for discipline_code in disciplines_list:
                 classes_info = disciplines_list[discipline_code]
