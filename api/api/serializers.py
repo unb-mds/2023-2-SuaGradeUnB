792021d891899d915c0bc5ae86e162c2a8a704be
GabrielCastelo-31 <gabflubr@outlook.com>
2023-12-05 22:47:08 -0300

    api(schedule): fix repeated code in serializer - Added inheritance to avoid repeated code

--- api/api/serializers.py
+++ api/api/serializers.py
@@ -1,31 +1,29 @@
 from rest_framework.serializers import ModelSerializer
 from api.models import Department, Discipline, Class
 
+
 class DepartmentSerializer(ModelSerializer):
     class Meta:
         model = Department
         fields = '__all__'
+
+
 class ClassSerializer(ModelSerializer):
     class Meta:
         model = Class
         fields = '__all__'
 
-class DisciplineSerializer(ModelSerializer):
-    department = DepartmentSerializer()
-    classes = ClassSerializer(many=True)
-    
-    class Meta:
-        model = Discipline
-        fields = '__all__'
 
 class DisciplineSerializerSchedule(ModelSerializer):
     class Meta:
         model = Discipline
         fields = '__all__'
 
-class ClassSerializerSchedule(ModelSerializer):
+
+class DisciplineSerializer(DisciplineSerializerSchedule):
+    department = DepartmentSerializer()
+    classes = ClassSerializer(many=True)
+
+
+class ClassSerializerSchedule(ClassSerializer):
     discipline = DisciplineSerializerSchedule()
-    
-    class Meta:
-        model = Class
-        fields = '__all__'
