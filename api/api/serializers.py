78f887793a0d11b0bde69adc20cf0e9a546823c9
mateuvrs <mateusvrs.dev@gmail.com>
2023-12-07 11:43:51 -0300

    refactor(api): add espaço entre classe e função

--- api/api/serializers.py
+++ api/api/serializers.py
@@ -16,6 +16,7 @@ class ClassSerializer(ModelSerializer):
 
 class DisciplineSerializerSchedule(ModelSerializer):
     department = DepartmentSerializer()
+
     class Meta:
         model = Discipline
         fields = '__all__'
