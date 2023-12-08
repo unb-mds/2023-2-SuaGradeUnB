caf3a3c074a5931f04d27def69709218cf4dabb1
mateuvrs <mateusvrs.dev@gmail.com>
2023-12-08 01:57:09 -0300

    web(comp): arruma nome do setModal

--- web/app/components/Modal/Modal.tsx
+++ web/app/components/Modal/Modal.tsx
@@ -1,23 +1,19 @@
 import useModal from '@/app/hooks/useModal';
-import useWindowDimensions from '@/app/hooks/useWindowDimensions';
-
-import { isMobile } from '../AsideSchedulePopUp/Tooltip';
 
 interface ModalPropsType {
     children: React.ReactNode;
 }
 
 export default function Modal({ children }: ModalPropsType) {
-    const { width } = useWindowDimensions();
-    const { setActiveModel } = useModal();
+    const { setActiveModal } = useModal();
 
     return (
         <div className="flex justify-center items-center fixed bg-snow-primary bg-opacity-[55%] h-full w-full top-0 left-0 z-10">
-            <div className="relative overflow-x-auto flex flex-col gap-1 justify-center items-start rounded-md p-5 pb-8 bg-blue-500 mx-2">
+            <div className="relative overflow-auto h-[87%] w-11/12 rounded-lg bg-snow-tertiary mx-2">
                 {children}
                 <button
-                    onClick={() => setActiveModel(false)}
-                    className={`absolute left-2 ${isMobile(width) ? 'bottom-2' : 'top-2'} material-symbols-rounded`}
+                    onClick={() => setActiveModal(false)}
+                    className='absolute left-2 top-2 material-symbols-rounded'
                 >
                     close
                 </button>
--- web/app/contexts/ModalContext.tsx
+++ web/app/contexts/ModalContext.tsx
@@ -4,7 +4,7 @@ import { createContext, useState } from 'react';
 
 interface ModalContextType {
     activeModel: boolean;
-    setActiveModel: React.Dispatch<React.SetStateAction<boolean>>;
+    setActiveModal: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const ModalContext = createContext({} as ModalContextType);
@@ -12,10 +12,10 @@ export const ModalContext = createContext({} as ModalContextType);
 export default function ModalContextProvider({ children }: {
     children: React.ReactNode;
 }) {
-    const [activeModel, setActiveModel] = useState(true);
+    const [activeModel, setActiveModal] = useState(true);
 
     return (
-        <ModalContext.Provider value={{ activeModel, setActiveModel }}>
+        <ModalContext.Provider value={{ activeModel, setActiveModal }}>
             {children}
         </ModalContext.Provider>
     );
