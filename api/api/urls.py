722c3c9a53b103aef560366774aecb6f8384b066
Caio <caiofeliperro@gmail.com>
2023-12-08 00:22:11 -0300

    api(delete-schedules): delete schedule endpoint

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from api.views import save_schedule, get_schedules, views
+from api.views import save_schedule, get_schedules, delete_schedule, views
 
 app_name = 'api'
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path('year-period/', views.YearPeriod.as_view(), name="year-period"),
     path('schedule/save/', save_schedule.SaveSchedule.as_view(), name="save-schedule"),
     path('schedule/', views.Schedule.as_view(), name="schedule"),
-    path('schedules/', get_schedules.GetSchedules.as_view(), name="get-schedules")
+    path('schedules/', get_schedules.GetSchedules.as_view(), name="get-schedules"),
+    path('schedules/<int:id>/', delete_schedule.DeleteSchedule.as_view(), name="delete-schedule")
 ]
