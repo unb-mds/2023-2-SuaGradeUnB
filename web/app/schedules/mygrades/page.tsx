95f566e464422938b3edcfbd1714a87d99c0af65
mateuvrs <mateusvrs.dev@gmail.com>
2023-12-08 01:59:11 -0300

    web(mygrades): adiciona test index 0 p

--- web/app/schedules/mygrades/page.tsx
+++ web/app/schedules/mygrades/page.tsx
@@ -2,11 +2,14 @@
 
 import Modal from '@/app/components/Modal/Modal';
 import Schedule from '@/app/components/Schedule/Schedule';
+
 import useModal from '@/app/hooks/useModal';
+import useSchedules from '@/app/hooks/useSchedules';
 
 import useUser from '@/app/hooks/useUser';
 
 export default function MyGrades() {
+    const { localSchedules } = useSchedules();
     const { activeModel } = useModal();
     const { user } = useUser();
 
@@ -14,7 +17,7 @@ export default function MyGrades() {
         <>
             {activeModel &&
                 <Modal>
-                    <Schedule />
+                    <Schedule schedules={localSchedules[0]} />
                 </Modal>
             }
         </>
