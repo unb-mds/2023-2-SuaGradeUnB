9c66ab271b56a89f3a865c4bc82347e6cae0c2cc
Caio <caiofeliperro@gmail.com>
2023-12-08 16:39:34 -0300

    api(urls): update urls

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from api.views import save_schedule, get_schedules, delete_schedule, views
+from api.views import schedules, delete_schedule, views
 
 app_name = 'api'
 
 urlpatterns = [
     path('', views.Search.as_view(), name="search"),
     path('year-period/', views.YearPeriod.as_view(), name="year-period"),
-    path('schedule/save/', save_schedule.SaveSchedule.as_view(), name="save-schedule"),
-    path('schedule/', views.Schedule.as_view(), name="schedule"),
-    path('schedules/', get_schedules.GetSchedules.as_view(), name="get-schedules"),
-    path('schedules/<int:id>/', delete_schedule.DeleteSchedule.as_view(), name="delete-schedule")
+    path('schedules/', schedules.Schedules.as_view(), name="schedules"),
+    path('schedules/<int:id>/', delete_schedule.DeleteSchedule.as_view(), name="delete-schedule"),
+    path('schedules/generate/', views.GenerateSchedule.as_view(), name="generate-schedules"),
 ]
