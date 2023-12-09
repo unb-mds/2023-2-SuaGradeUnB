f8d28d99e8e539057269796090252401379707ab
Arthur Ribeiro <artrsousa1@gmail.com>
2023-12-09 20:30:18 -0300

    web(fix): improving MyGrades function

--- web/app/schedules/mygrades/page.tsx
+++ web/app/schedules/mygrades/page.tsx
@@ -4,34 +4,52 @@ import useSchedules from '@/app/hooks/useSchedules';
 
 import SchedulePreview from '@/app/components/SchedulePreview/SchedulePreview';
 
+export function RenderLocalSchedules(){
+    const { localSchedules } = useSchedules();
+    if(!localSchedules.length) return null;
+
+    return (
+        <>
+            <h1 className='text-center pb-5'>Grades locais</h1>
+            <div className='flex flex-wrap justify-center items-center gap-10 w-11/12 mx-auto'>
+                {localSchedules.map((schedule, index) =>
+                    schedule && <SchedulePreview key={index} index={index} localSchedule={schedule} />
+                )}
+            </div>
+        </>
+    
+    );
+}
+
+export function RenderCloudSchedules(){
+    const { cloudSchedules } = useSchedules();
+    if(!cloudSchedules.length) return null;
+
+    return(
+        <>
+            <h1 className={`text-center pb-5 ${cloudSchedules.length ? 'pt-16' : ''}`}>Grades nuvem</h1>
+            <div className='flex flex-wrap justify-center items-center gap-16 w-11/12 mx-auto'>
+                {cloudSchedules.map((schedule, index) =>
+                    schedule && <SchedulePreview key={index} index={index} cloudSchedule={schedule} isCloud />
+                )}
+            </div>
+        </>
+    );
+}
+
 export default function MyGrades() {
     const { localSchedules, cloudSchedules } = useSchedules();
 
     return (
-        localSchedules.length || cloudSchedules.length ?
-            <div className='overflow-auto min-h-full max-h-full px-3'>
-                <h1 className='text-center text-xl font-semibold pb-6'>Suas Grades</h1>
-                <div>
-                    {localSchedules.length ? <h1 className='text-center pb-5'>Grades locais</h1> : null}
-                    <div className='flex flex-wrap justify-center items-center gap-10 w-11/12 mx-auto'>
-                        {localSchedules.length ?
-                            localSchedules.map((schedule, index) =>
-                                schedule && <SchedulePreview key={index} index={index} localSchedule={schedule} />
-                            ) : null
-                        }
-                    </div>
-                    {cloudSchedules.length ? <h1 className={`text-center pb-5 ${localSchedules.length ? 'pt-16' : ''}`}>Grades nuvem</h1> : null}
-                    <div className='flex flex-wrap justify-center items-center gap-16 w-11/12 mx-auto'>
-                        {cloudSchedules.length ?
-                            cloudSchedules.map((schedule, index) =>
-                                schedule && <SchedulePreview key={index} index={index} cloudSchedule={schedule} isCloud />
-                            ) : null
-                        }
-                    </div>
+        <div className='overflow-auto min-h-full max-h-full px-3'>
+            <h1 className='text-center text-xl font-semibold pb-6'>Suas Grades</h1>
+            <RenderLocalSchedules />
+            <RenderCloudSchedules />
+            {localSchedules.length === 0 && cloudSchedules.length === 0 && (
+                <div className='flex h-full justify-center items-center'>
+                    Você ainda não possui nenhuma grade.
                 </div>
-            </div> :
-            <div className='flex h-full justify-center items-center'>
-                Você ainda não possui nenhuma grade.
-            </div>
+            )}
+        </div>
     );
 }
