3cfefcbb7251c04e0794ac5f1f5b2eea3a1c10a8
mateuvrs <mateusvrs.dev@gmail.com>
2023-12-26 20:18:28 -0300

    api(commands): add cache verification to update db

--- api/api/management/commands/updatedb.py
+++ api/api/management/commands/updatedb.py
@@ -1,8 +1,10 @@
 from typing import Any
 from argparse import ArgumentParser as CommandParser
 from django.core.management.base import BaseCommand
-from utils import sessions, web_scraping
-from utils.db_handler import delete_classes_from_discipline, delete_all_departments_using_year_and_period, get_or_create_department, get_or_create_discipline, create_class
+from utils import sessions
+from utils import db_handler as dbh
+from utils.web_scraping import DisciplineWebScraper, get_list_of_departments
+from django.core.cache import cache
 from time import time
 
 
@@ -13,8 +15,11 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser: CommandParser) -> None:
         """Adiciona os argumentos do comando."""
-        parser.add_argument('-a', '-all', action='store_true', dest='all', default=False,
+        parser.add_argument('-a', '--all', action='store_true', dest='all', default=False,
                             help="Atualiza o banco de dados com as disciplinas dos períodos atual e seguinte.")
+        
+        parser.add_argument('-ds', '--descriptive', action='store_true', default=False,
+                    help="Ativa a opção de uma atualização descritiva com os outputs (print) necessários")
 
         parser.add_argument('-p', '--period', action='store', default=None,
                             choices=[".".join(sessions.get_current_year_and_period()), ".".join(
@@ -37,11 +42,11 @@ class Command(BaseCommand):
             print("Utilize o comando 'updatedb -h' para mais informações.")
             return
 
-        # Obtem o ano e o período anterior ao período atual
+        # Obtém o ano e o período anterior ao período atual
         previous_period_year, previous_period = sessions.get_previous_period()
 
         # Apaga as disciplinas do período anterior
-        delete_all_departments_using_year_and_period(
+        dbh.delete_all_departments_using_year_and_period(
             year=previous_period_year, period=previous_period)
 
         if options["delete"]:
@@ -49,7 +54,7 @@ class Command(BaseCommand):
                 self.delete_period(year=year, period=period)
             return
 
-        departments_ids = web_scraping.get_list_of_departments()
+        departments_ids = get_list_of_departments()
 
         if departments_ids is None:
             self.display_error_message("department_ids")
@@ -60,44 +65,64 @@ class Command(BaseCommand):
         for year, period in choices:
             try:
                 start_time = time()
-                self.update_departments(
-                    departments_ids=departments_ids, year=year, period=period)
+                print(f"Começando atualização de {year}/{period}")
+                self.update_departments(departments_ids, year, period, options)
                 self.display_success_update_message(
                     operation=f"{year}/{period}", start_time=start_time)
             except Exception as exception:
                 print("Houve um erro na atualização do bando de dados.")
                 print(f"Error: {exception}")
 
-    def update_departments(self, departments_ids: list, year: str, period: str) -> None:
+    def update_departments(self, departments_ids: list, year: str, period: str, options: Any) -> None:
         """Atualiza os departamentos do banco de dados e suas respectivas disciplinas."""
         for department_id in departments_ids:
-            print(f"WebScraping do departamento: {department_id}")
-            disciplines_list = web_scraping.get_department_disciplines(
-                department_id=department_id, current_year=year, current_period=period)
-            department = get_or_create_department(
+            scraper = DisciplineWebScraper(department_id, year, period)
+            fingerprint = scraper.create_page_fingerprint()
+
+            try:
+                cache_key = f"{department_id}/{year}.{period}"
+                cache_value = cache.get(cache_key)
+                if cache_value and cache_value == fingerprint:
+                    if options['descriptive']:
+                        print(f"Departamento ({department_id}) atualizado, operação não necessária")
+                    continue
+
+                THIRTY_DAYS = 30 * 86400
+                cache.set(cache_key, fingerprint, timeout=THIRTY_DAYS)
+            except:
+                print("Ocorreu um erro ao tentar acessar o cache")
+                pass
+
+            disciplines_list = scraper.get_disciplines()
+            department = dbh.get_or_create_department(
                 code=department_id, year=year, period=period)
 
-            print("Atualizando disciplinas do departamento...")
+            if options['descriptive']:
+                print(f"Departamento ({department_id}) desatualizado, operação necessária")
             # Para cada disciplina do período atual, deleta as turmas previamente cadastradas e cadastra novas turmas no banco de dados
             for discipline_code in disciplines_list:
                 classes_info = disciplines_list[discipline_code]
                 # Cria ou pega a disciplina
-                discipline = get_or_create_discipline(
+                discipline = dbh.get_or_create_discipline(
                     name=classes_info[0]["name"], code=discipline_code, department=department)
 
                 # Deleta as turmas previamente cadastradas
-                delete_classes_from_discipline(discipline=discipline)
+                dbh.delete_classes_from_discipline(discipline=discipline)
 
                 # Cadastra as novas turmas
                 for class_info in classes_info:
-                    create_class(teachers=class_info["teachers"],
-                                 classroom=class_info["classroom"], schedule=class_info["schedule"],
-                                 days=class_info["days"], _class=class_info["class_code"], discipline=discipline, special_dates=class_info["special_dates"])
+                    dbh.create_class(teachers=class_info["teachers"],
+                                     classroom=class_info["classroom"], schedule=class_info["schedule"],
+                                     days=class_info["days"], _class=class_info["class_code"], discipline=discipline, special_dates=class_info["special_dates"])
+                    
+            if options['descriptive']:
+                print(f'Operação de atualização finalizada para o departamento ({department_id})')
 
     def delete_period(self, year: str, period: str) -> None:
         """Deleta um período do banco de dados."""
         start_time = time()
-        delete_all_departments_using_year_and_period(year=year, period=period)
+        dbh.delete_all_departments_using_year_and_period(
+            year=year, period=period)
         self.display_success_delete_message(
             operation=f"{year}/{period}", start_time=start_time)
 
