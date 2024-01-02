c95d8708f1b99b565ce91ca8129e40e46012c442
Mateus Vieira <68292695+mateusvrs@users.noreply.github.com>
2024-01-02 01:07:47 -0300

    fix(api): arruma função de ano/período atual que tinha incosistência nos dias 30/31 de dez (#186)
    * test(utils): check if boundaries of func works well
    
    * fix(utils): correct 'if' func return at 30/31 dec

--- api/utils/tests/test_date_utils.py
+++ api/utils/tests/test_date_utils.py
@@ -2,11 +2,12 @@ from rest_framework.test import APITestCase
 from utils import sessions as sns
 from datetime import datetime
 
+
 class DateUtilsTest(APITestCase):
 
     def compare_date(self, date: datetime):
-        before_dec = datetime(date.year, 12, 30) >= date
-        after_may = datetime(date.year, 5, 1) <= date
+        before_dec = datetime(date.year + 1, 1, 1) >= date
+        after_may = datetime(date.year, 5, 2) <= date
 
         return before_dec and after_may
 
@@ -14,14 +15,48 @@ class DateUtilsTest(APITestCase):
         month = 2 if self.compare_date(date) else 6
         return datetime(date.year + self.compare_date(date), month=month, day=1)
 
-    def test_get_current_year_and_period(self):
-        year, period = sns.get_current_year_and_period()
+    def test_get_current_year_and_period_with_right_may_limits(self):
+        year, period = sns.get_current_year_and_period(datetime(2023, 5, 1))
 
-        date = datetime.now()
-        self.assertEqual(year, str(date.year))
+        self.assertEqual("2023", year)
+        self.assertEqual("1", period)
 
-        self.assertEqual((period == '2'), self.compare_date(date))
-        self.assertEqual((period == '1'), not self.compare_date(date))
+        year, period = sns.get_current_year_and_period(datetime(2023, 5, 2))
+
+        self.assertEqual("2023", year)
+        self.assertEqual("2", period)
+    
+    def test_get_current_year_and_period_with_left_jan_limits(self):
+        year, period = sns.get_current_year_and_period(datetime(2023, 12, 31))
+
+        self.assertEqual("2023", year)
+        self.assertEqual("2", period)
+
+        year, period = sns.get_current_year_and_period(datetime(2024, 1, 1))
+
+        self.assertEqual("2024", year)
+        self.assertEqual("1", period)
+    
+    def test_get_current_year_and_period_with_middle_date(self):
+        year, period = sns.get_current_year_and_period(datetime(2023, 2, 18))
+
+        self.assertEqual("2023", year)
+        self.assertEqual("1", period)
+
+        year, period = sns.get_current_year_and_period(datetime(2023, 9, 20))
+
+        self.assertEqual("2023", year)
+        self.assertEqual("2", period)
+
+        year, period = sns.get_current_year_and_period(datetime(2024, 4, 23))
+
+        self.assertEqual("2024", year)
+        self.assertEqual("1", period)
+
+        year, period = sns.get_current_year_and_period(datetime(2024, 7, 9))
+
+        self.assertEqual("2024", year)
+        self.assertEqual("2", period)
 
     def test_get_next_period(self):
         _, period = sns.get_next_period()
@@ -70,4 +105,3 @@ class DateUtilsTest(APITestCase):
         year, period = sns.get_previous_period(date)
 
         self.assertEqual(str(date.year - (period == '2')), year)
-
