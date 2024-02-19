f4c4adbd0d4787aeeaca7f7487269b587f51b38a
mateusvrs <mateusvrs.dev@gmail.com>
2024-02-19 16:54:40 -0300

    front(home): divide selected classes by section

--- web/app/schedules/home/page.tsx
+++ web/app/schedules/home/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { Fragment, useEffect, useRef } from 'react';
 import useSelectedClasses from '@/app/hooks/useSelectedClasses';
 import useClassesToShow from '@/app/hooks/useClassesToShow';
 
@@ -12,6 +12,34 @@ import AddDisciplineButton from './components/AddDisciplineButton';
 import ClickOutsideHandler from './handlers/ClickOutsideHandler';
 
 import ShowPopUpContentContextProvider from '@/app/contexts/ShowPopUpContentContext';
+
+import { ClassValueType } from '@/app/contexts/SelectedClassesContext/types';
+
+function DisciplineBlockJSX({ ...props }: {
+    index: number,
+    cls: ClassValueType,
+    classesToShow: ClassValueType[]
+}) {
+    let classAppearance;
+    for (let indexInner = 0; indexInner < props.classesToShow.length; indexInner++) {
+        if (props.classesToShow[indexInner].discipline.code === props.cls.discipline.code) {
+            classAppearance = indexInner;
+            break;
+        }
+    }
+
+    return (
+        <Fragment key={props.index}>
+            {props.index === classAppearance ?
+                <span className='flex justify-center items-center rounded-md font-medium bg-primary bg-opacity-80 w-10/12 h-8'>
+                    {props.cls.discipline.name}
+                </span>
+                : null
+            }
+            <DisciplineBox currentClass={props.cls} discipline={props.cls.discipline} />
+        </Fragment>
+    );
+}
 
 export default function Home() {
     const { classesToShow, setClassesToShow } = useClassesToShow();
@@ -31,9 +59,9 @@ export default function Home() {
             <ClickOutsideHandler refs={{ divAddClassRef, buttonAddDisciplineRef }}>
                 <AddDisciplineButton buttonAddDisciplineRef={buttonAddDisciplineRef} />
                 <div className={`flex flex-col items-center ${classesToShow.length ? '' : 'justify-center text-center'} gap-5 overflow-auto min-h-full max-h-full pb-2`}>
-                    {classesToShow.length ? classesToShow.map((cls, index) =>
-                        <DisciplineBox key={index} currentClass={cls} discipline={cls.discipline} />
-                    ) : 'Nenhuma disciplina escolhida no momento'}
+                    {classesToShow.length ? classesToShow.map((cls, index) => {
+                        return DisciplineBlockJSX({ index, cls, classesToShow });
+                    }) : 'Nenhuma disciplina escolhida no momento'}
                 </div>
 
                 <AsideSchedulePopUp divAddClassRef={divAddClassRef} />
