db0d26118632922db180215f6151a43a810adb5e
Lucas Queiroz <lucas.fn4@gmail.com>
2024-07-22 22:38:48 -0300

    adds tests to frontend with jest

---
+++ web/app/contexts/UserContexts.spec.ts
@@ -0,0 +1,9 @@
+import { renderHook } from '@testing-library/react'
+import UserContextProvider from './UserContext.tsx'
+
+describe('test', () => {
+    it('testa', () => {
+        expect(1).toBe(1)
+    })
+})
+
