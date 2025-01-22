ce429ca43761bee65b88fc4a1fa9e3fd790f5b96
Caio <caiofeliperro@gmail.com>
2025-01-22 13:31:36 -0300

    feat(#232): atualiza comando de atualização
    * Adiciona opção de apagar departamentos não especificados
    * Adiciona opção de escolher número de threads para atualização
    * Apaga períodos que não possuem disciplinas
    * Melhora sistema descritivo de logs do comando
    * Formata código utilizando black

--- api/api/management/commands/updatedb.py
+++ api/api/management/commands/updatedb.py
@@ -16,55 +16,118 @@ class Command(BaseCommand):
     """Comando para atualizar o banco de dados."""
 
     help = "Atualiza o banco de dados com as disciplinas do SIGAA e suas respectivas turmas."
+    descriptive: bool = False
+
+    def descript(self, message: str) -> None:
+        if self.descriptive:
+            print(message)
 
     def add_arguments(self, parser: CommandParser) -> None:
         """Adiciona os argumentos do comando."""
-        parser.add_argument('-a', '--all', action='store_true', dest='all', default=False,
-                            help="Atualiza o banco de dados com as disciplinas dos períodos atual e seguinte.")
+        parser.add_argument(
+            "-a",
+            "--all",
+            action="store_true",
+            dest="all",
+            default=False,
+            help="Atualiza o banco de dados com as disciplinas dos períodos válidos para pesquisa.",
+        )
 
-        parser.add_argument('-ds', '--descriptive', action='store_true', default=False,
-                            help="Ativa a opção de uma atualização descritiva com os outputs (print) necessários")
+        parser.add_argument(
+            "-ds",
+            "--descriptive",
+            action="store_true",
+            default=False,
+            help="Ativa a opção de uma atualização descritiva com os outputs (print) necessários",
+        )
 
-        parser.add_argument('-p', '--period', action='store', default=None,
-                            choices=[".".join(sessions.get_current_year_and_period()), ".".join(
-                                sessions.get_next_period())],
-                            dest='period', help="Atualiza o banco de dados com as disciplinas do período especificado.")
+        parser.add_argument(
+            "-p",
+            "--period",
+            action="store",
+            default=None,
+            choices=[sessions.get_year_and_period_in_range()],
+            dest="period",
+            help="Atualiza o banco de dados com as disciplinas do período especificado.",
+        )
 
-        parser.add_argument('-d', '--delete', action='store_true', dest='delete', default=False,
-                            help="Deleta o período especificado do banco de dados.")
+        parser.add_argument(
+            "-d",
+            "--delete",
+            action="store_true",
+            dest="delete",
+            default=False,
+            help="Deleta o período especificado do banco de dados.",
+        )
+
+        parser.add_argument(
+            "-do",
+            "--delete-others",
+            action="store_true",
+            dest="delete_others",
+            default=False,
+            help="Deleta todos os períodos exceto o período especificado do banco de dados.",
+        )
+
+        parser.add_argument(
+            "-t",
+            "--threads",
+            action="store",
+            type=int,
+            default=3,
+            dest="threads",
+            help="Número de threads para a execução do comando.",
+        )
 
     def handle(self, *args: Any, **options: Any):
         choices = []
         threads = []
 
         if options["all"]:
-            choices.append(sessions.get_current_year_and_period())
-            choices.append(sessions.get_next_period())
+            choices.extend(
+                list(
+                    map(
+                        lambda py: sessions.get_year_and_period(py),
+                        sessions.get_year_and_period_in_range(),
+                    )
+                )
+            )
         elif options["period"] is not None:
-            choices.append(options["period"].split("."))
+            choices.append(sessions.get_year_and_period(options["period"]))
         else:
             print("Nenhum período foi especificado.")
             print("Utilize o comando 'updatedb -h' para mais informações.")
             return
+        
+        if options["delete"] and options["delete_others"]:
+            print("Não é possível deletar e deletar outros períodos ao mesmo tempo")
+            return
 
-        # Obtém o ano e o período anterior ao período atual
-        previous_period_year, previous_period = sessions.get_previous_period()
+        self.descriptive = options["descriptive"]
 
-        # Apaga as disciplinas do período anterior
-        dbh.delete_all_departments_using_year_and_period(
-            year=previous_period_year, period=previous_period)
-
-        if options["delete"]:
-            for year, period in choices:
-                thread = threading.Thread(
-                    target=self.delete_period, args=(year, period,))
-                threads.append(thread)
-                thread.start()
+        # Deleta o período especificado do banco de dados
+        if options["delete"] or options["delete_others"]:
+            years_and_periods = dbh.get_all_years_and_periods(use_cache=False) if options["delete_others"] else choices
             
-            for thread in threads:
-                thread.join()
-            threads.clear()
+            for index in range(0, len(years_and_periods), options["threads"]):
+                threads = []
+                for year, period in years_and_periods[index : index + options["threads"]]:
+                    thread = threading.Thread(
+                        target=self.delete_period,
+                        args=(
+                            year,
+                            period,
+                        ),
+                    )
+                    threads.append(thread)
+                    thread.start()
+                    sleep(0.01) # little time to start print don't overleap
 
+                for thread in threads:
+                    thread.join()
+                threads.clear()
+
+            cache.delete("years_and_periods")
             return
 
         departments_ids = get_list_of_departments()
@@ -80,22 +143,34 @@ class Command(BaseCommand):
                 start_time = time()
                 print(f"Começando atualização de {year}/{period}")
                 with transaction.atomic():
-                    self.update_departments(
-                        departments_ids, year, period, options)
+                    self.update_departments(departments_ids, year, period, options)
 
                 self.display_success_update_message(
-                    operation=f"{year}/{period}", start_time=start_time)
+                    operation=f"{year}/{period}", start_time=start_time
+                )
             except Exception as exception:
                 print("Houve um erro na atualização do banco de dados.")
                 print(f"Error: {exception}")
 
         start_tot_time = time()
-        for year, period in choices:
-            thread = threading.Thread(
-                target=start_update_year_period, args=(year, period,))
-            threads.append(thread)
-            thread.start()
-            sleep(0.01)  # little time to start print don't overleap
+
+        for index in range(0, len(choices), options["threads"]):
+            threads = []
+            for year, period in choices[index : index + options["threads"]]:
+                thread = threading.Thread(
+                    target=start_update_year_period,
+                    args=(
+                        year,
+                        period,
+                    ),
+                )
+                threads.append(thread)
+                thread.start()
+                sleep(0.01) # little time to start print don't overleap
+
+            for thread in threads:
+                thread.join()
+            threads.clear()
 
         print()
 
@@ -103,10 +178,15 @@ class Command(BaseCommand):
             thread.join()
         threads.clear()
 
+        self.delete_empty_departments(choices)
+        cache.delete("years_and_periods")
         print(f"\nTempo total de execução: {(time() - start_tot_time):.1f}s")
 
-    def update_departments(self, departments_ids: list, year: str, period: str, options: Any) -> None:
+    def update_departments(
+        self, departments_ids: list, year: str, period: str, options: Any
+    ) -> None:
         """Atualiza os departamentos do banco de dados e suas respectivas disciplinas."""
+
         def execute_update(department_id):
             scraper = DisciplineWebScraper(department_id, year, period)
             fingerprint = scraper.create_page_fingerprint()
@@ -115,8 +195,9 @@ class Command(BaseCommand):
             try:
                 cache_value = cache.get(cache_key)
                 if cache_value and cache_value == fingerprint:
-                    if options['descriptive']:
-                        print(f"Departamento ({department_id}) atualizado, operação não necessária")
+                    self.descript(
+                        f"Departamento ({department_id}) atualizado, operação não necessária"
+                    )
                     return
             except:
                 print("Ocorreu um erro ao tentar acessar o cache")
@@ -124,36 +205,47 @@ class Command(BaseCommand):
 
             disciplines_list = scraper.get_disciplines()
             department = dbh.get_or_create_department(
-                code=department_id, year=year, period=period)
+                code=department_id, year=year, period=period
+            )
 
-            if options['descriptive']:
-                print(f"Departamento ({department_id}) desatualizado, operação necessária")
+            self.descript(
+                f"Departamento ({department_id}) desatualizado, operação necessária"
+            )
 
             # Para cada disciplina do período atual, deleta as turmas previamente cadastradas e cadastra novas turmas no banco de dados
             for discipline_code in disciplines_list:
                 classes_info = disciplines_list[discipline_code]
                 # Cria ou pega a disciplina
                 discipline = dbh.get_or_create_discipline(
-                    name=classes_info[0]["name"], code=discipline_code, department=department)
+                    name=classes_info[0]["name"],
+                    code=discipline_code,
+                    department=department,
+                )
 
                 # Deleta as turmas previamente cadastradas
                 dbh.delete_classes_from_discipline(discipline=discipline)
 
                 # Cadastra as novas turmas
                 for class_info in classes_info:
-                    dbh.create_class(teachers=class_info["teachers"],
-                                     classroom=class_info["classroom"], schedule=class_info["schedule"],
-                                     days=class_info["days"], _class=class_info["class_code"], discipline=discipline, special_dates=class_info["special_dates"])
+                    dbh.create_class(
+                        teachers=class_info["teachers"],
+                        classroom=class_info["classroom"],
+                        schedule=class_info["schedule"],
+                        days=class_info["days"],
+                        _class=class_info["class_code"],
+                        discipline=discipline,
+                        special_dates=class_info["special_dates"],
+                    )
 
             cache.set(cache_key, fingerprint, timeout=THIRTY_DAYS_IN_SECS)
-            
-            if options['descriptive']:
-                print(f'Operação de atualização finalizada para o departamento ({department_id})')
-            
+
+            self.descript(
+                f"Operação de atualização finalizada para o departamento ({department_id})"
+            )
+
         threads = deque()
         for department_id in departments_ids:
-            thread = threading.Thread(
-                target=execute_update, args=(department_id,))
+            thread = threading.Thread(target=execute_update, args=(department_id,))
             threads.append(thread)
             thread.start()
 
@@ -169,10 +261,18 @@ class Command(BaseCommand):
         """Deleta um período do banco de dados."""
         start_time = time()
         with transaction.atomic():
-            dbh.delete_all_departments_using_year_and_period(
-                year=year, period=period)
+            dbh.delete_all_departments_using_year_and_period(year=year, period=period)
         self.display_success_delete_message(
-            operation=f"{year}/{period}", start_time=start_time)
+            operation=f"{year}/{period}", start_time=start_time
+        )
+    
+    def delete_empty_departments(self, years_and_periods: list[tuple[str, str]]) -> None:
+        """Deleta os departamentos vazios do banco de dados."""
+        for year, period in years_and_periods:
+            disciplines = dbh.filter_disciplines_by_year_and_period(year=year, period=period)
+            
+            if not disciplines.count():
+                self.delete_period(year, period)
 
     def display_error_message(self, operation: str) -> None:
         print("Não foi possível realizar a operação de atualização do banco de dados.")
